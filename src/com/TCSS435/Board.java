a12c368800fd1bb946fdd20702dc4564939a59c4
fordm13@uw.edu <husky6tcss360@gmail.com>
2016-07-29 20:26:37 -0700

    Board now has a toString and pieces can be placed and quads can be rotated.

--- src/com/TCSS435/Board.java
+++ src/com/TCSS435/Board.java
@@ -1,12 +1,19 @@
 package com.TCSS435;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by Mike on 7/26/16.
  */
 public class Board{
 
+    private final char EMPTY = '.', WHITE = 'w', BLACK = 'b';
+    private final ArrayList<Integer> QUADS = new ArrayList<>(Arrays.asList(1,2,3,4));
+    private final String OCCUPIED = "That space isn't available";
     private final int START = 0;
     private final int MIDDLE = 3;
+    private final int BOARD_DIM = 3;
     private final int END = 6;
 
     private char[][] myQ1;
@@ -18,15 +25,19 @@ public class Board{
     private int myBs;
     private int myWs;
 
-    public Board(char[][] theState){
-        if(theState.length != 6){
-            System.out.println("That board is the wrong size!");
-        } else {
-            myQ1 = getQuadrant(theState, START, MIDDLE, START, MIDDLE);
-            myQ2 = getQuadrant(theState, START, MIDDLE, MIDDLE, END);
-            myQ3 = getQuadrant(theState, MIDDLE, END, START, MIDDLE);
-            myQ4 = getQuadrant(theState, MIDDLE, END, MIDDLE, END);
-            myState = theState;
+    public Board(){
+
+        myQ1 = new char[BOARD_DIM][BOARD_DIM];
+        myQ2 = new char[BOARD_DIM][BOARD_DIM];
+        myQ3 = new char[BOARD_DIM][BOARD_DIM];
+        myQ4 = new char[BOARD_DIM][BOARD_DIM];
+        for(int i = 0; i < BOARD_DIM; i++){
+            for(int j = 0; j < BOARD_DIM; j++){
+                myQ1[i][j] = EMPTY;
+                myQ2[i][j] = EMPTY;
+                myQ3[i][j] = EMPTY;
+                myQ4[i][j] = EMPTY;
+            }
         }
     }
 
@@ -62,22 +73,139 @@ public class Board{
         return myQ4;
     }
 
+    /**
+     * A method that will update the position of a spot on the board.
+     *
+     * @param quadrant
+     * @param row
+     * @param col
+     * @param b
+     */
+    public void changeSpace(int quadrant, int row, int col, char b){
+        if(row < 0 || row > 2 || col < 0 || col > 2){
+            System.out.println("That's not a valid space, try again");
+            System.out.println(this.toString());
+        }else if(b != WHITE && b!= BLACK){
+            System.out.println("NOT A VALID PIECE!");
+            System.out.println(this.toString());
+        }else{
+            if(QUADS.contains(quadrant)){
+                if(quadrant == 1){
+                    if(myQ1[row][col] == EMPTY){
+                        myQ1[row][col] = b;
+                    }else {
+                        System.out.println(OCCUPIED);
+                    }
+                } else if(quadrant == 2){
+                    if(myQ2[row][col] == EMPTY){
+                        myQ2[row][col] = b;
+                    }else {
+                        System.out.println(OCCUPIED);
+                    }
+                } else if(quadrant == 3){
+                    if(myQ3[row][col] == EMPTY){
+                        myQ3[row][col] = b;
+                    }else {
+                        System.out.println(OCCUPIED);
+                    }
+                } else if(quadrant == 4){
+                    if(myQ4[row][col] == EMPTY){
+                        myQ4[row][col] = b;
+                    }else {
+                        System.out.println(OCCUPIED);
+                    }
+                }
+
+            }else{
+                System.out.println("That's not a valid QUADRANT");
+            }
+        }
+
+    }
+
+    /**
+     * Roatates the given quadrant Left.
+     *
+     * @param quad the quad to rotate
+     */
+    public void rotateLeft(int quad){
+        if(QUADS.contains(quad)){
+            if(quad == 1){
+                rotateL(myQ1);
+            }else if(quad == 2){
+                rotateL(myQ2);
+            } else if(quad == 3){
+                rotateL(myQ3);
+            } else if(quad == 4){
+                rotateL(myQ4);
+            }
+        } else {
+            System.out.println("");
+        }
+    }
+
+    public void rotateRight(int quad){
+        if(QUADS.contains(quad)){
+            if(quad == 1){
+                rotateR(myQ1);
+            }else if(quad == 2){
+                rotateR(myQ2);
+            } else if(quad == 3){
+                rotateR(myQ3);
+            } else if(quad == 4){
+                rotateR(myQ4);
+            }
+        } else {
+            System.out.println("");
+        }
+    }
+
+    private void rotateL(char[][] quad){
+        char[][] temp = makeCopy(quad);
+        for(int i = 0 ; i < quad.length; i++){
+            for(int j = 0; j < quad.length; j++){
+                quad[i][j] = temp[j][quad.length - i - 1];
+            }
+        }
+    }
+
+    private void rotateR(char[][] quad){
+        char[][] temp = makeCopy(quad);
+        for(int i = 0 ; i < quad.length; i++){
+            for(int j = 0; j < quad.length; j++){
+                quad[i][j] = temp[quad.length - j - 1][i];
+            }
+        }
+    }
+
+
+    private char[][] makeCopy(char[][] toCopy){
+        char[][] copy = new char[toCopy.length][toCopy.length];
+        for(int i = 0; i < toCopy.length; i++){
+            for(int j = 0; j < toCopy.length; j++){
+                copy[i][j] = toCopy[i][j];
+            }
+        }
+        return copy;
+    }
     @Override
     public String toString(){
         String divder = "+-------+-------+\n";
         StringBuilder output = new StringBuilder(divder);
-        for(int i = 0; i < myState.length; i++){
-            for(int j = 0; j < myState.length; j++){
-                if(j == 0 || j == myState.length/2){
-                    output.append("|");
-                } else if(i == myState.length/2){
-                    output.append(divder);
-                }
-                output.append(myState[i][j]);
-            }
-        }
+        output.append("| " + myQ1[0][0] + " " + myQ1[0][1] + " "+ myQ1[0][2] + " | " + myQ2[0][0] + " " + myQ2[0][1] + " " + myQ2[0][2] + " |\n");
+        output.append("| " + myQ1[1][0] + " " + myQ1[1][1] + " "+ myQ1[1][2] + " | " + myQ2[1][0] + " " + myQ2[1][1] + " " + myQ2[1][2] + " |\n");
+        output.append("| " + myQ1[2][0] + " " + myQ1[2][1] + " "+ myQ1[2][2] + " | " + myQ2[2][0] + " " + myQ2[2][1] + " " + myQ2[2][2] + " |\n");
         output.append(divder);
+        output.append("| " + myQ3[0][0] + " " + myQ3[0][1] + " "+ myQ3[0][2] + " | " + myQ4[0][0] + " " + myQ4[0][1] + " " + myQ4[0][2] + " |\n");
+        output.append("| " + myQ3[1][0] + " " + myQ3[1][1] + " "+ myQ3[1][2] + " | " + myQ4[1][0] + " " + myQ4[1][1] + " " + myQ4[1][2] + " |\n");
+        output.append("| " + myQ3[2][0] + " " + myQ3[2][1] + " "+ myQ3[2][2] + " | " + myQ4[2][0] + " " + myQ4[2][1] + " " + myQ4[2][2] + " |\n");
+        output.append(divder);
+
 
         return output.toString();
     }
+
+
+
+
 }
--- src/com/TCSS435/Main.java
+++ src/com/TCSS435/Main.java
@@ -3,6 +3,15 @@ package com.TCSS435;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        boolean done = false;
+	    Board newB = new Board();
+        System.out.println(newB.toString());
+        newB.changeSpace(1, 1, 2, 'w');
+        newB.changeSpace(1, 0, 0, 'b');
+        newB.changeSpace(1, 2, 1, 'w');
+        System.out.println(newB.toString());
+        newB.rotateLeft(1);
+        System.out.println(newB.toString());
     }
 }
