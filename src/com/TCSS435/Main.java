f656342830b0ff89b0b3332f82d52711f4f077d4
fordm13@uw.edu <husky6tcss360@gmail.com>
2016-07-30 08:41:22 -0700

    Board now has a toString and pieces can be placed and quads can be rotated.

--- src/com/TCSS435/Main.java
+++ src/com/TCSS435/Main.java
@@ -1,11 +1,22 @@
 package com.TCSS435;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-
-        boolean done = false;
+    public static void main(String[] args) throws IOException {
+        String file = "results.txt";
+        FileWriter fw = new FileWriter(file, true);
+        PrintWriter outputFile = new PrintWriter(fw);
+        PrintStream out = new PrintStream(new FileOutputStream("result.txt"));
+        System.setOut(out);
+        Scanner input = new Scanner(System.in);
+        boolean gameOver = false;
 	    Board newB = new Board();
+        System.out.println("HOW MANY PLAYERS? [1 or 2]");
+
+        System.out.println();
         System.out.println(newB.toString());
         newB.changeSpace(1, 1, 2, 'w');
         newB.changeSpace(1, 0, 0, 'b');
@@ -14,4 +25,5 @@ public class Main {
         newB.rotateLeft(1);
         System.out.println(newB.toString());
     }
+
 }
